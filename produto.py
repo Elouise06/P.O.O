f1c0125620060dad9cada99f7028e923bd700889
Elouise06 <elouise.costa@escolar.ifrn.edu.br>
2024-07-16 20:33:32 -0300

    Atividade Protudo

--- produto.py
+++ produto.py
@@ -4,5 +4,10 @@ class Protudo:
         self.preço = preço
         self.quantidade = quantidade
 
-protudo1 = Protudo()
-        
+protudo1 = Protudo("Notebook", 3800.00, 10)
+print("Protudo 1: \nNome:{} \nPreço:{} \nQuantidade em estoque:{}".format(protudo1.nome, protudo1.preço, protudo1.quantidade))
+
+print()
+
+protudo2 = Protudo("Mouse", 50.00, 100)
+print("Protudo 2: \nNome:{} \nPreço:{} \nQuantidade em estoque:{}".format(protudo2.nome, protudo2.preço, protudo2.quantidade))
