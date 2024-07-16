5776666375d159cffe3fb2e3d72423f8303b3e48
Elouise da Costa Galdino <20231041110030@ifrn.local>
2024-07-16 17:26:09 -0300

    aula_poo

--- livro.py
+++ livro.py
@@ -1,11 +1,11 @@
 class Livro:
-    def __init__(self):
-        self.titulo = ""
-        self.autor = ""
-        self.ano = ""
+    def __init__(self, titulo, autor, ano):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
 
-livro1 = Livro()
-livro1.titulo = "1984"
-livro1.autor = "george Orwell"
-livro1.ano = "1984"
+livro1 = Livro("1984", "george Orwell", "1984")
 print("O livro {} foi escrito por {} em {}".format(livro1.titulo, livro1.autor, livro1.ano))
+
+livro_elouise = Livro("o mundo de sofia",  "Jostein Gaarder",  "1991")
+print("O livro {} foi escrito por {} em {}".format(livro_elouise.titulo, livro_elouise.autor, livro_elouise.ano))
