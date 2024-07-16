e419f6d11e891f7876f4e7789628671797df175c
Elouise da Costa Galdino <20231041110030@ifrn.local>
2024-07-16 17:55:19 -0300

    aluno_poo

---
+++ aluno.py
@@ -0,0 +1,8 @@
+class Aluno:
+    def __init__(self, nome, curso, serie):
+        self.nome = nome
+        self.curso = curso
+        self.serie = serie
+
+aluno1 = Aluno("Maria José", "Informática", "1o ano")
+print()
---
+++ veiculo.py
@@ -0,0 +1,11 @@
+class veiculo:
+    def __init__(self, modelo, marca, ano):
+        self.modelo = modelo
+        self.marca = marca
+        self.ano = ano
+
+carro1 = veiculo('voyage', 'Volkswagen', 2015)
+print("O carro de modelo {}, da marca {}, foi lançado em {} ".format(carro1.modelo, carro1.marca, carro1.ano))
+
+carro2 = veiculo('Ford Ka', 'Ford', 2018)
+print("O carro de modelo {}, da marca {}, foi lançado em {} ".format(carro2.modelo, carro2.marca, carro2.ano))
