a85438005f9e1413e4eb948444bd61a69e442bb2
Elouise da Costa Galdino <20231041110030@ifrn.local>
2024-07-17 17:53:35 -0300

    Atividade cofrinho

--- cofrinho.py
+++ cofrinho.py
@@ -10,8 +10,19 @@ class Cofrinho:
         x = self.meta - self.valor
         print("falta {} para os {} da sua meta".format(x, self.meta))
 
+    def falta(self):
+        if self.valor == self.meta:
+            print("Parabéns você chegou a sua meta!")
+        elif self.valor > self.meta:
+            print("Você ultrapassou a sua meta!")
+        else:
+            x = self.meta - self.valor
+            print("falta {} para os {} da sua meta".format(x, self.meta))
+
 meta_ise= Cofrinho(7000, 20000)
 meta_ise.adicionar(400)
 meta_ise.falta()
 meta_ise.adicionar(350)
+meta_ise.falta()
+meta_ise.adicionar(15000)
 meta_ise.falta()
