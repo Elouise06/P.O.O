9636297520d1ecb1fa477a2777c000f5a51677a2
Elouise da Costa Galdino <20231041110030@ifrn.local>
2024-09-03 17:10:28 -0300

    Exercício 1: Biblioteca e Livros

--- relação entre objetos/Exercício 1 Biblioteca e Livros/livro.py
+++ relação entre objetos/Exercício 1 Biblioteca e Livros/livro.py
@@ -1,24 +1,27 @@
 from autor import Autor
+
 class Livro:
-    def __init__(self, titulo, autor, anoPublicacao):
+    def __init__(self, titulo: str, autor: Autor, ano_publicacao: int):
         self.__titulo = titulo
         self.__autor = autor
-        self.__anoPublicacao = anoPublicacao
+        self.__ano_publicacao = ano_publicacao
 
-    def setTitulo(self, titulo):
-        self.__titulo = titulo
-
-    def setautor(self, autor):
-        self.__autor = autor
-
-    def setAnoPublicacao(self, anoPublicacao):
-        self.__anoPublicacao = anoPublicacao
-
-    def getTitulo(self):
+    # Getters
+    def get_titulo(self) -> str:
         return self.__titulo
 
-    def getAutor(self):
+    def get_autor(self) -> Autor:
         return self.__autor
 
-    def getAnoPublicacao(self):
-        return self.__anoPublicacao
+    def get_ano_publicacao(self) -> int:
+        return self.__ano_publicacao
+
+    # Setters
+    def set_titulo(self, titulo: str):
+        self.__titulo = titulo
+
+    def set_autor(self, autor: Autor):
+        self.__autor = autor
+
+    def set_ano_publicacao(self, ano_publicacao: int):
+        self.__ano_publicacao = ano_publicacao
